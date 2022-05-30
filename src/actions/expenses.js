afb2f731913e39f87825e9a999d8996f6fa34536
LennyW95 <lenkawrnatova@gmail.com>
2022-05-30 13:23:32 +0200

    Removing from firebase fix

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -59,7 +59,7 @@ export const startSetExpenses = () => {
     };
 };
 
-export const startRemoveExpense = ({ id = {} }) => {
+export const startRemoveExpense = ({ id } = {}) => {
     return (dispatch) => {
         return database.ref(`expenses/${id}`).remove().then(() => {
             dispatch(removeExpense({ id }));
